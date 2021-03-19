ef84573c8d516db62efcc65b4466eddafe1b0f52
Joseph Robinson <smartech56@gmail.com>
2021-03-18 23:27:21 -0700

    Added Spring Project Files

---
+++ src/test/java/net/josephworks/WebJava/WebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.josephworks.WebJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
